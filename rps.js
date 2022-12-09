9a873017a30cb5f4b28365246d5d7542e3932c38
Mike <104871211+MN724@users.noreply.github.com>
2022-12-09 18:42:13 -0500

    Created ui for rps and reworking code

--- rps.js
+++ rps.js
@@ -1,54 +1,81 @@
 function getComputerChoice() {
-  let ComputerValue = Math.floor(Math.random() * 3);
-  
-  return ComputerValue;
-}
-
-ComputerValue = getComputerChoice();
-
-if (ComputerValue == 0) {
-  ComputerChoice = "rock";
-} else if (ComputerValue == 1) {
-  ComputerChoice = "paper";
-} else if (ComputerValue == 2) {
-  ComputerChoice = "scissors";
-}
-
-let UserChoice = prompt("rock, paper, or scissors?");
-UserChoice = UserChoice.toLowerCase();
-// let UserChoice = "Rock";
-
-if (UserChoice == "rock") {
-  UserValue = 0;
-} else if (UserChoice == "paper") {
-  UserValue = 1;
-} else if (UserChoice == "scissors") {
-  UserValue = 2;
-}
-
-if (ComputerValue == UserValue) {
-  console.log("Tie");
-} else if (ComputerValue == 0) {
-  if (UserValue == 1) {
-    console.log("You Win! Paper beats Rock");
-  } else if (UserValue == 2) {
-    console.log("You Lose! Rock beats Scissors");
+  let ComputerValue = Math.floor(Math.random() * 3 + 1);
+  if (ComputerValue == 1) {
+    ComputerChoice = "Rock";
+    console.log("Computer chose Rock");
+  } else if (ComputerValue == 2) {
+    ComputerChoice = "Scissors";
+    console.log("Computer chose Scissors");
+  } else if (ComputerValue == 3) {
+    ComputerChoice = "Paper";
+    console.log("Computer chose Paper");
   }
-} else if (ComputerValue == 1) {
-  if (UserValue == 2) {
-    console.log("You Win! Scissors beats Paper");
-  } else if (UserValue == 0) {
-    console.log("You Lose! Paper beats Rock");
-  }
-} else if (ComputerValue == 2) {
-  if (UserValue == 0) {
-    console.log("You Win! Rock beats Scissors");
-  } else if (UserValue == 1) {
-    console.log("You Lose! Scissors beats Paper");
+  return ComputerChoice;
+}
+
+function playRound(UserChoice, ComputerChoice) {
+  if (ComputerChoice === "Rock") {
+    if (UserChoice === "Rock") {
+      result = "Tie! Both chose rock!";
+      console.log(result);
+    } else if (UserChoice === "Paper") {
+      result = "You win! Paper beats rock!";
+      console.log(result);
+    } else if (UserChoice === "Scissors") {
+      result = "You Lose! Rock beats scissors!";
+      console.log(result);
+    }
+  } else if (ComputerChoice === "Paper") {
+    if (UserChoice === "Rock") {
+      result = "You Lose! Paper beats rock!";
+      console.log(result);
+    } else if (UserChoice === "Paper") {
+      result = "Tie! Both chose Paper!";
+      console.log(result);
+    } else if (UserChoice === "Scissors") {
+      result = "You win! Scissors beats paper!";
+      console.log(result);
+    }
+  } else if (ComputerChoice === "Scissors") {
+    if (UserChoice === "Rock") {
+      result = "You win! Rock beats Scissors!";
+      console.log(result);
+    } else if (UserChoice === "Paper") {
+      result = "You Lose! Scissors beats paper!";
+      console.log(result);
+    } else if (UserChoice === "Scissors") {
+      result = "Tie! Both chose scissors!";
+      console.log(result);
+    }
   }
 }
 
-console.log(ComputerValue);
-console.log(ComputerChoice);
-console.log(UserValue);
-console.log(UserChoice);
+
+
+let UserChoice = "";
+let ComputerChoice = "";
+
+const rock_btn = document.querySelector('.rock-btn');
+rock_btn.addEventListener('click', () => {
+  UserChoice = "rock";
+  console.log("You chose rock");
+})
+
+const paper_btn = document.querySelector('.paper-btn');
+paper_btn.addEventListener('click', () => {
+  UserChoice = "paper";
+  console.log("You chose paper");
+})
+
+const scissors_btn = document.querySelector('.scissors-btn');
+scissors_btn.addEventListener('click', () => {
+  UserChoice = "scissors";
+  console.log("You chose scissors");
+})
+
+ComputerChoice = getComputerChoice();
+
+const play_btn = document.querySelector('.user-input-btns');
+play_btn.addEventListener('click', function() {
+  playRound(UserChoice, ComputerChoice);
+});
