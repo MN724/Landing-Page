340c02662e1c83a0472a3ac8e804636a8a21dd74
Mike <104871211+MN724@users.noreply.github.com>
2022-12-10 21:30:37 -0500

    Created html css and js files for calculator page. Created addition, subtraction, multiplication, and division functions

---
+++ calculatorScript.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return x = a + b;
+}
+
+function subtract(a, b) {
+  return x = a - b;
+}
+
+function multiply(a, b) {
+  return x = a * b;
+}
+
+function divide(a, b) {
+  return x = a / b;
+}
