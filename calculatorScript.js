732c64e02e6080dc2ce6d633324d9e6783624e94
Mike <104871211+MN724@users.noreply.github.com>
2022-12-10 22:31:01 -0500

    Added html and css to create basic layout for calculator

--- calculatorScript.js
+++ calculatorScript.js
@@ -13,3 +13,17 @@ function multiply(a, b) {
 function divide(a, b) {
   return x = a / b;
 }
+
+function operate(a, b, op) {
+  if (op === "+") {
+    return add(a, b);
+  } else if (op === "-") {
+    return subtract(a, b);
+  } else if (op === "*") {
+    return multiply(a, b);
+  } else if (op === "/") {
+    return divide(a, b);
+  }
+}
+
+console.log(operate(10, 5, "+"));
