cb29f58f4d01a4600f34d88dc7d35e68bf1d0d73
Mike <104871211+MN724@users.noreply.github.com>
2022-12-12 21:04:50 -0500

    Working on logic for calculator

--- calculatorScript.js
+++ calculatorScript.js
@@ -26,7 +26,7 @@ function calculate(a, b) {
 
 }
 
-function operate(a, b, op) {
+function operate(a, op, b) {
   if (op === "clear") {
     ans = clear();
   } else if (op === "+") {
@@ -42,6 +42,7 @@ function operate(a, b, op) {
   }
 
   popDisplay(ans);
+  return ans;
 }
 
 function popDisplay(ans) {
@@ -52,6 +53,8 @@ function popDisplay(ans) {
 var ans = 0;
 var num = 0;
 
+
+const zero = document.getElementById('0');
 const one = document.getElementById('1');
 const two = document.getElementById('2');
 const three = document.getElementById('3');
@@ -61,7 +64,8 @@ const six = document.getElementById('6');
 const seven = document.getElementById('7');
 const eight = document.getElementById('8');
 const nine = document.getElementById('9');
-const zero = document.getElementById('0');
+
+const numButtons = [zero, one, two, three, four, five, six, seven, eight, nine]
 
 const clearDisp = document.getElementById('clear');
 const addDisp = document.getElementById('add');
@@ -70,88 +74,134 @@ const multDisp = document.getElementById('mult');
 const divDisp = document.getElementById('div');
 const evalDisp = document.getElementById('eval');
 
+const exp = [0, "+", 0];
+
+// var numArray = [];
+// var Numbers = [];
+// var Operators = [];
+// var Answers = [];
 
 
-var numArray = [];
-var Numbers = [];
-var Operators = [];
-var Answers = [];
-
-function concateNum (numArray){
-  var fullNum = '';
-  numArray.forEach ((num) =>
-  {
-    fullNum = fullNum + num;
-  });
-  var fullNum = Number(fullNum);
-  popDisplay(fullNum);
-  return fullNum;
-}
+// function concateNum (numArray){
+//   var fullNum = '';
+//   numArray.forEach ((num) =>
+//   {
+//     fullNum = fullNum + num;
+//   });
+//   var fullNum = Number(fullNum);
+//   popDisplay(fullNum);
+//   return fullNum;
+// }
 
 one.onclick = () => {
-  numArray.push(1);
-  num = concateNum(numArray);
+
 }
 two.onclick = () => {
-  numArray.push(2);
-  num = concateNum(numArray);
+
 }
 three.onclick = () => {
-  numArray.push(3);
-  num = concateNum(numArray);
+
 }
-four.onclick = () => {
-  numArray.push(4);
-  num = concateNum(numArray);
-}
-five.onclick = () => {
-  numArray.push(5);
-  num = concateNum(numArray);
-}
-six.onclick = () => {
-  numArray.push(6);
-  num = concateNum(numArray);
-}
-seven.onclick = () => {
-  numArray.push(7);
-  num = concateNum(numArray);
-}
-eight.onclick = () => {
-  numArray.push(8);
-  num = concateNum(numArray);
-}
-nine.onclick = () => {
-  numArray.push(9);
-  num = concateNum(numArray);
-}
-zero.onclick = () => {
-  numArray.push(0);
-  num = concateNum(numArray);
-}
-dec.onclick = () => {
-  numArray.push(".");
-  num = concateNum(numArray);
-}
+
+numButtons.forEach ((numButton, index) => {
+  numButton.onclick = () => {
+    exp[2] += index.toString();
+    exp[2] = Number(exp[2]);
+    console.log(exp);
+    popDisplay(exp[2]);
+  }
+})
+
+// one.onclick = () => {
+//   numArray.push(1);
+//   num = concateNum(numArray);
+// }
+// two.onclick = () => {
+//   numArray.push(2);
+//   num = concateNum(numArray);
+// }
+// three.onclick = () => {
+//   numArray.push(3);
+//   num = concateNum(numArray);
+// }
+// four.onclick = () => {
+//   numArray.push(4);
+//   num = concateNum(numArray);
+// }
+// five.onclick = () => {
+//   numArray.push(5);
+//   num = concateNum(numArray);
+// }
+// six.onclick = () => {
+//   numArray.push(6);
+//   num = concateNum(numArray);
+// }
+// seven.onclick = () => {
+//   numArray.push(7);
+//   num = concateNum(numArray);
+// }
+// eight.onclick = () => {
+//   numArray.push(8);
+//   num = concateNum(numArray);
+// }
+// nine.onclick = () => {
+//   numArray.push(9);
+//   num = concateNum(numArray);
+// }
+// zero.onclick = () => {
+//   numArray.push(0);
+//   num = concateNum(numArray);
+// }
+// dec.onclick = () => {
+//   numArray.push(".");
+//   num = concateNum(numArray);
+// }
 
 clearDisp.onclick = () => [ans, numArray, num] = clear();
 addDisp.onclick = () => {
-  operate(ans, num, "+");
-  Numbers.push(num);
-  Operators.push("+");
+  ans = operate(exp[0], exp[1], exp[2]);
+  exp[0] = ans;
+  exp[1] = "+";
+  exp [2] = 0;
+  console.log(ans);
+  console.log(exp);
 }
 subDisp.onclick = () => {
-  operate(ans, num, "-");
-  Numbers.push(num);
-  Operators.push("-");
+  ans = operate(exp[0], exp[1], exp[2]);
+  exp[0] = ans;
+  exp[1] = "-";
+  exp [2] = 0;
+  console.log(ans);
+  console.log(exp);
 }
 multDisp.onclick = () => {
-  operate(ans, num, "*");
-  Numbers.push(num);
-  Operators.push("*");
+  ans = operate(exp[0], exp[1], exp[2]);
+  exp[0] = ans;
+  exp[1] = "*";
+  exp [2] = 0;
+  console.log(ans);
+  console.log(exp);
 }
 divDisp.onclick = () => {
-  operate(ans, num, "/");
-  Numbers.push(num);
-  Operators.push("/");
+  ans = operate(exp[0], exp[1], exp[2]);
+  exp[0] = ans;
+  exp[1] = "/";
+  exp [2] = 0;
+  console.log(ans);
+  console.log(exp);
 }
-evalDisp.onclick = () => operate(ans, num, "=");
+evalDisp.onclick = () => {
+  ans = operate(exp[0], exp[1], exp[2]);
+  exp[0] = ans;
+  exp[1] = "=";
+  exp [2] = 0;
+  console.log(ans);
+  console.log(exp);
+}
+
+clearDisp.onclick = () => {
+  exp[0] = 0;
+  exp[1] = "+";
+  exp [2] = 0;
+  popDisplay(0);
+}
